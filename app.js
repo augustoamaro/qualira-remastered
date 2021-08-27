49357bd3246e1cb6761a77d03be9f644f01d98b7
unknown <augustoamaro@icloud.com>
2021-08-26 22:43:05 -0300

    Working with b tag

--- app.js
+++ app.js
@@ -2,55 +2,52 @@ function spinWheel() {
     const x = 1024;
     const y = 9999;
     const deg = Math.floor(Math.random() * (x - y)) + y;
-
+    
     const rotate = document.getElementById('box').style.transform = "rotate(" + deg + "deg)";
-
+    
     var element = document.getElementById('mainbox');
     element.classList.remove('animate');
     setTimeout(function () {
         element.classList.add('animate');
     }, 5000);
-
+    
     generateQualira()
-
+    
 }
 
 
 function generateQualira() {
-
     
-    const monkeyName = ['Pira Kids', 'Anatoile', 'KratosPhp', 'Lucão do Java', 'Señor Developer', 'Leònidas', 'Menino Neitan', 'Felipão'];
-    const array = monkeyName.values()
+    const bs = document.querySelectorAll('[slice]')
+    const monkeyName = ['Pira Kids', 'Anatoile', 'KratosPhp', 'Lucão do Java', 'Señor Developer', 'Leònidas', 'Menino Neitan', 'Felipão'];    
 
-    for (let names of array) {
-        const resultado = document.querySelectorAll('[teste]').innerText = names
-        console.log(resultado); // parou aqui
-    }
+    //Pegar todos os spans
+    //Pegar todos os nomes
+    //Juntar tudo
 
-    // for (b of spans) {
-    //     b.innerText = monkeyName
+    monkeyName.forEach(function(el, index) {
+        
+    })
+    
+     
+    
+    
+
+    // const randomMonkeyName = monkeyName[(Math.random() * monkeyName.length) | 0]
+    // document.querySelector('[randomName]').innerText = randomMonkeyName
+
+    // const randomNumber = (Math.random() * 101)
+    // const roundNumber = parseFloat(randomNumber.toFixed(0))
+
+    // if (roundNumber > 51) {
+
+    //     document.querySelector('[randomNumber]').innerText = `Você é um FUCKINNNN QUALIRA, com ${roundNumber}% de qualiragem`
+
+    // } else {
+
+    //     document.querySelector('[randomNumber]').innerText = `Você não é um qualira, muito bem!`
     // }
 
-    const randomMonkeyName = monkeyName[(Math.random() * monkeyName.length) | 0]
-    document.querySelector('[randomName]').innerText = randomMonkeyName
-
-    const randomNumber = (Math.random() * 101)
-    const roundNumber = parseFloat(randomNumber.toFixed(0))
-
-    if (roundNumber > 51) {
-
-        document.querySelector('[randomNumber]').innerText = `Você é um FUCKINNNN QUALIRA, com ${roundNumber}% de qualiragem`
-
-    } else {
-
-        document.querySelector('[randomNumber]').innerText = `Você não é um qualira, muito bem!`
-    }
-
 }
-
-
-// Arrumar 
-// Cair confete ao aparecer o nome
-// Tocar musica I Will Survive
 
 
